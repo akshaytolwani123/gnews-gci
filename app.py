cff4397cb7271a8c722900a210c86f3b585e1b5f
akhay_tolwani <akhaytolwanipython@gmail.com>
2020-01-12 08:43:43 +0530

    Added sort news

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from flask import Flask, render_template,abort
-from newsapi imprt NewsApiClient
+from newsapi import NewsApiClient
 
 app = Flask(__name__)
 
+newsapi = NewsApiClient(api_key='0cae0ef493f1426ba9b13ec730ce8019')
 @app.route('/')
 def news():
-    news = newsapi(q='Open-Source OR Android OR Linux',)
+    news = newsapi.get_everything(q='Open-Source OR Android OR Linux',language = 'en',sort_by = 'publishedAt' )
     return render_template('home.html',news = news)
-    
+
+if __name__ == '__main__':
+    app.run()
